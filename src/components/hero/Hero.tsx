50116ada35dd29c39f834417111c5eb3df06faa0
Beri28 <cletusberinyuy@gmail.com>
2025-05-09 10:56:33 +0100

    first commit

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,12 @@
+import './hero.css'
+const Hero = () => {
+    return ( 
+        <div className="h-[80vh] hero">
+            <div className="h-[100%] bg-[#00000080]">
+            
+            </div>
+        </div>
+     );
+}
+ 
+export default Hero;
