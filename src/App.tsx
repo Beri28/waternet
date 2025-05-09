a266ed56f48bf48744812132b6314d4d8c1fa637
NgwaNathan <nathanngwa3@gmail.com>
2025-05-09 14:41:38 -0700

    first comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import HomeScreen from './Pages/home_page/homepage'
+import PhoneQRGenerator  from './Pages/home_page/qr_code/qr_code_generator';
+import { FiBell } from 'react-icons/fi';
 // import Home from './pages/home/Home'
 
 function App() {
@@ -7,7 +10,7 @@ function App() {
 
   return (
     <>
-      Hello
+      <PhoneQRGenerator/>
     </>
   )
 }
