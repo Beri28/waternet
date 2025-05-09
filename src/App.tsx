fa99b555feea919a5e2b6f63fbbbfaec22a7a84d
CpNelly <nebanelly3@gmail.com>
2025-05-09 23:43:02 +0100

    merge resolved

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import HomeScreen from './Pages/home_page/homepage'
 import PhoneQRGenerator  from './Pages/home_page/qr_code/qr_code_generator';
-import { FiBell } from 'react-icons/fi';
 import WithdrawalPage from './Pages/MoneyManagementPages/WithdrawalPage'
 
 // import Home from './pages/home/Home'
